95076b287137863cd0ace35a4c4717c3bc56fdbe
wksnders <42388072+wksnders@users.noreply.github.com>
2024-08-02 22:49:10 -0600

    Paddle go vroom, but as sprite now

--- breakout.js
+++ breakout.js
@@ -1,51 +1,90 @@
 //single quote for default strings
 //convention to end lines;
-//var Vector = require("vector2js");
+
 //get canvas
 var canvas = document.getElementById('game-canvas'); 
-var {screenWidth, screenHeight} = canvas;
+var {width, height} = canvas;
 var context = canvas.getContext('2d');
 //other render context is web gl, dont make raw web gl
 
 //mdn documentation
-/*
+
 var isPaused = true;
 var isGameOver = false;
 
 var lives = 3;
 var highScore = 0;
-var score = 0;*/
+var score = 0;
 
 
 //bricks = [];
-// paddle = nil
-// ball = nil
 
 var rightPressed = false;
 var leftPressed = false;
 
 
 //listen for dom event mouse point mv
-
+class sprite {
+    constructor(id,height,width){
+        this.id = id;
+        this.active = true;
+        this.positionX = 0;
+        this.positionY = 0;
+        this.rotation = 0;
+        this.height = height;
+        this.width = width;
+        this.velocityX = 0;
+        this.velocityY = 0;
+        this.angVel = 0
+    }
+    setActive(active){
+        this.active = active;
+    }
+    setPosition(x,y){
+        this.positionX = x;
+        this.positionY = y;
+    }
+    setRotation(rotation){
+        this.rotation = rotation;
+    }
+    setSize(height,width){
+        this.height = height;
+        this.width = width;
+    }
+    Update(){
+        this.setPosition(
+            this.positionX + (deltaTime * sprite.velocityX),
+            this.positionY + (deltaTime * sprite.velocityY)
+        );
+        if(angVel != 0){
+            this.setRotation(
+                sprite.rotation + (sprite.angVel * deltaTime)
+            );
+        }
+    }
+}
 
 //ms since landed on page  (float)
-var paddleX = 0;
-var paddleY = 360;
-var paddleWidth = 100;
-var paddleHeight = 10;
+const paddleStartX = 0;
+const paddleStartY = 360;
+var paddle = new sprite('paddle',10,100);
+paddle.setPosition(paddleStartX,paddleStartY);
+
 var paddleXSpeed = 250;
-/*
-const paddleStart = new Vector(0, 360)
-const ballStart = new Vector(0, 330)
+
+const ballStartX = 0;
+const ballStartY = 330;
+var ball = new sprite('ball',20,20);
 
 //called once
 var onInitialize = function(){
     //setup inputs
     
     //create paddle
+    //var paddle = new sprite('paddle',10,100);
     
     //create ball
-    
+    //ball = new sprite('ball',20,20);
     //
     OnGameStart();
 }
@@ -57,10 +96,12 @@ var OnGameStart = function(){
     //CreateBricks()
 
     //position paddle
+    paddle.setPosition(paddleStartX,paddleStartY);
     //position ball
+    ball.setPosition(ballStartX,ballStartY)
 
     //set ball to not move yet
-}*/
+}
 
 //rendering loop
 var lastTime = 0;
@@ -82,17 +123,17 @@ var vsyncLoop = function (time) {
     console.log('vsyncLoop : deltaTime',deltaTime);
 
     context.fillStyle = '#0002';
-    context.fillRect(0, 0, screenWidth, screenHeight);
+    context.fillRect(0, 0, width, height);
 
-    paddleX += paddleXSpeed * deltaTime;
-    paddleX %= width;
+    paddle.positionX += paddleXSpeed * deltaTime;
+    paddle.positionX %= width;
 
     context.fillStyle = '#FFF';
     context.fillRect(
-        paddleX - (paddleWidth/2),
-        paddleY,
-        paddleWidth,
-        paddleHeight
+        paddle.positionX - (paddle.width/2),
+        paddle.positionY,
+        paddle.width,
+        paddle.height
     );
 
     //empty
@@ -104,41 +145,7 @@ requestAnimationFrame(vsyncLoop);
 
 
 
-/*
-class sprite {
-    constructor(id,height,width){
-        this.id = id;
-        this.active = true;
-        this.position = new Vector(0, 0);
-        this.rotation = 0;
-        this.height = height;
-        this.width = width;
-        this.velocity = new Vector(0, 0);
-        this.angVel = 0
-    }
-    setActive(active){
-        this.active = active;
-    }
-    setPosition(position){
-        this.position = position;
-    }
-    setRotation(rotation){
-        this.rotation = rotation;
-    }
-    setSize(height,width){
-        this.height = height;
-        this.width = width;
-    }
-    Update(){
-        this.setPosition(
-            sprite.position + (deltaTime * sprite.velocity)
-        );
-        if(angVel != 0){
-            this.setRotation(
-                sprite.rotation + (sprite.angVel * deltaTime)
-            );
-        }
-    }
-}*/
+
+
 
 
